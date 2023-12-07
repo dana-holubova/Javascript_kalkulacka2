641083acfc50a7f2f0a6d5147b2777649e873ee6
dana-holubova <danmich@email.cz>
2023-12-07 14:21:25 +0100

    Část úkolu.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const display = document.querySelector(".display")
+const keypad = document.querySelector(".keypad")
 
+const handleDigitClick = (e) => {
+const key = e.target.textContent
+display.textContent += key
+}
+
+//console.log('display content2: ' + display.textContent)
+//console.log('display length2: ' + display.textContent.length)
+
+//nefunguje - nenačítá to aktuální hodnoty z displeje
+if (display.textContent.length < 10) {
+keypad.addEventListener("click", handleDigitClick)   
+}
